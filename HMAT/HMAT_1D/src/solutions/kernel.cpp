7d11ac7da95ab22f69f4d4e19f5fbe397fa4b47e
Daniele Casati <daniele.casati@sam.math.ethz.ch>
2017-11-08 19:58:02 +0100

    hmat problems finished, plots to do

--- HMAT/HMAT_1D/src/solutions/kernel.cpp
+++ HMAT/HMAT_1D/src/solutions/kernel.cpp
@@ -3,6 +3,7 @@
 #include <limits>
 
 // Kernel functor $\frac{num}{|x-y|}$ if $x != y$, else 0
+/* SAM_LISTING_BEGIN_0 */
 double KernelInvDistance::operator()(double x, double y)
 {
     if(std::abs(x-y) > std::numeric_limits<double>::epsilon())
@@ -10,8 +11,10 @@ double KernelInvDistance::operator()(double x, double y)
     else
         return 0.;
 }
+/* SAM_LISTING_END_0 */
 
 // Kernel functor \f$\frac{\cos(C\left|x-y\right|)}{\left|x-y\right|}\f$ if $x != y$, else 0
+/* SAM_LISTING_BEGIN_1 */
 double KernelCosine::operator()(double x, double y)
 {
     if(std::abs(x-y) > std::numeric_limits<double>::epsilon())
@@ -19,3 +22,4 @@ double KernelCosine::operator()(double x, double y)
     else
         return 0.;
 }
+/* SAM_LISTING_END_1 */
