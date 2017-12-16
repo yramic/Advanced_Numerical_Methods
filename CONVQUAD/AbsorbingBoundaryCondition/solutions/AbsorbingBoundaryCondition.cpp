df007a2e9ea2a2b11c7f47d0ef48eab7a56276c9
Daniele Casati <daniele.casati@sam.math.ethz.ch>
2017-12-16 14:43:47 +0100

    final changes of convquad

--- CONVQUAD/AbsorbingBoundaryCondition/solutions/AbsorbingBoundaryCondition.cpp
+++ CONVQUAD/AbsorbingBoundaryCondition/solutions/AbsorbingBoundaryCondition.cpp
@@ -9,17 +9,48 @@ using namespace Eigen;
 using namespace std;
 
 
+/* @brief Compute the convolution weights for Laplace transform F
+ * \param F Template function for the Laplace transform
+ * \param n Number of convolution weights, minus 1
+ * \param p Order of quadrature rule
+ * \param r Radius of circumference of integration (default = 1.0E-7)
+ * \\return Values of u at final time t = 1
+ */
+/* SAM_LISTING_BEGIN_0 */
+template<typename FFUNC>
+VectorXd conv_wght_bdf2(const FFUNC& F, size_t n, int p, double r=1.0E-7)
+{
+    double tau = 1./n;
+    VectorXcd w = VectorXd::Zero(n+1);
+
+    for(int i=0; i<p; ++i) {
+
+        // integrate on circumference centered in (0,0) with radius r:
+        complex<double> ti = r * complex<double>(cos(2.*M_PI*i/p), sin(2.*M_PI*i/p));
+
+        for(int l=0; l<n+1; ++l) {
+            w(l) += 1./sqrt(1.+tau*ti) * (1./pow(2.-sqrt(1.+2.*tau*ti),l+1) - 1./pow(2.+sqrt(1.+2.*tau*ti),l+1)) * F(ti);
+        }
+    }
+
+    w *= tau*r / complex<double>(0.,p); // coefficient of weight formula, tau/complex<double>(0.,2.*M\_PI), times quadrature weight, 2.*M\_PI*r/p
+
+    return w.real();
+}
+/* SAM_LISTING_END_0 */
+
+
 /* @brief Find the unknown function u at final time t = 1 in the evolution problem
  * using Galerkin discretization and convolution quadrature (BDF-2)
- * \param phi Template function for the right-hand side
+ * \param g Template function for the right-hand side
  * \param M Number of discretization intervals in time
  * \param N Number of discretization intervals in space
  * \param p Order of quadrature rule
  * \\return Values of u at final time t = 1
  */
-/* SAM_LISTING_BEGIN_0 */
+/* SAM_LISTING_BEGIN_1 */
 template<typename FUNC>
-VectorXd solveABC(const FUNC& phi, size_t M, size_t N, int p)
+VectorXd solveABC(const FUNC& g, size_t M, size_t N, int p)
 {
     double h = 1./N;
     VectorXd grid = VectorXd::LinSpaced(N+1,0.,1.);
@@ -35,26 +66,15 @@ VectorXd solveABC(const FUNC& phi, size_t M, size_t N, int p)
     A.insert(N-1,N) =-1./h + 1./(h*h*pow(M_PI,3))*(2.*cos(M_PI*(1.-h)) - 2. - M_PI*h*sin(M_PI*(1.-h)));
     A.insert(N,  N) = 1./h + 1./(h*h*pow(M_PI,3))*((M_PI*M_PI*h*h-2.)*cos(M_PI*(1.-h)) - 2. + 2.*M_PI*h*sin(M_PI*(1.-h)));
 
-    double tau = 1./M;
-    VectorXcd w = VectorXd::Zero(M+1);
-
-    for(int i=0; i<p; ++i) {
-
-        // integrate on circumference centered in (0,0) with radius $~ 10^{-7}$:
-        complex<double> ti = 1e-7*complex<double>(cos(2.*M_PI*i/p),sin(2.*M_PI*i/p));
-
-        for(int l=0; l<M+1; ++l) {
-            w(l) += 1./sqrt(1.+tau*ti) * (1./pow(2.-sqrt(1.+2.*tau*ti),l+1) - 1./pow(2.+sqrt(1.+2.*tau*ti),l+1)) * log(ti)/(ti*ti+1.);
-        }
-    }
-
-    w *= tau*1e-7/complex<double>(0.,p); // coefficient of weight formula, tau/complex<double>(0.,2.*M\_PI), times quadrature weight, 2.*M\_PI*1e-7/p
+    auto F = [](complex<double> s) { return log(s)/(s*s+1.); };
+    VectorXd w = conv_wght_bdf2(F, M, p);
 
     SparseMatrix<complex<double> > Aw = A.cast<complex<double> >();
     Aw.coeffRef(N,N) += w(0);
     SparseLU<SparseMatrix<complex<double> > > solver;
     solver.compute(Aw);
 
+    double tau = 1./M;
     MatrixXcd u = MatrixXcd::Zero(N+1,M+1); // first column is at time t = 0
     for(int i=1; i<M+1; ++i) {
 
@@ -65,7 +85,7 @@ VectorXd solveABC(const FUNC& phi, size_t M, size_t N, int p)
 
         VectorXcd phi_i(N+1);
         for(int j=0; j<N+1; ++j) {
-            phi_i(j) = (complex<double>)phi(grid(j),i*tau);
+            phi_i(j) = (complex<double>)g(grid(j), i*tau);
         }
 
         VectorXcd rhs = phi_i; rhs(N) -= rhs_N;
@@ -76,17 +96,17 @@ VectorXd solveABC(const FUNC& phi, size_t M, size_t N, int p)
 
     return u.col(M).real();
 }
-/* SAM_LISTING_END_0 */
+/* SAM_LISTING_END_1 */
 
 
 int main() {
-    /* SAM_LISTING_BEGIN_1 */
-    auto phi = [](double x, double t) { return sin(M_PI*x)*sin(M_PI*t); };
-    VectorXd u_ref = solveABC(phi, 2560, 2560, 20);
+    /* SAM_LISTING_BEGIN_2 */
+    auto g = [](double x, double t) { return sin(M_PI*x)*sin(M_PI*t); };
+    VectorXd u_ref = solveABC(g, 2560, 2560, 20);
 
     cout << "Problem 3.2.d" << endl;
     for(int N=10; N<=1280; N*=2) {
-        VectorXd u_tmp = solveABC(phi, 2560, N, 20);
+        VectorXd u_tmp = solveABC(g, 2560, N, 20);
         double error = 0.;
         double ratio = 2560/N;
         for(int i=1; i<=2560; ++i) {
@@ -100,7 +120,7 @@ int main() {
     }
 
     for(int M=10; M<=1280; M*=2) {
-        VectorXd u_tmp = solveABC(phi, M, 2560, 20);
+        VectorXd u_tmp = solveABC(g, M, 2560, 20);
         double error = 0.;
         for(int i=1; i<=2560; ++i) {
             error += pow((u_ref(i) - u_ref(i-1)) - (u_tmp(i) - u_tmp(i-1)), 2) * 2560.; // h = 1/N
@@ -110,5 +130,5 @@ int main() {
              << scientific << setprecision(10)
              << error << endl;
     }
-    /* SAM_LISTING_END_1 */
+    /* SAM_LISTING_END_2 */
 }
--- CONVQUAD/AbsorbingBoundaryCondition/templates/AbsorbingBoundaryCondition.cpp
+++ CONVQUAD/AbsorbingBoundaryCondition/templates/AbsorbingBoundaryCondition.cpp
@@ -9,25 +9,41 @@ using namespace Eigen;
 using namespace std;
 
 
+/* @brief Compute the convolution weights for Laplace transform F
+ * \param F Template function for the Laplace transform
+ * \param n Number of convolution weights, minus 1
+ * \param p Order of quadrature rule
+ * \param r Radius of circumference of integration (default = 1.0E-7)
+ * \\return Values of u at final time t = 1
+ */
+/* SAM_LISTING_BEGIN_0 */
+template<typename FFUNC>
+VectorXd conv_wght_bdf2(const FFUNC& F, size_t n, int p, double r=1.0E-7)
+{
+    // TODO: Compute the convolution weights for Laplace transform F
+}
+/* SAM_LISTING_END_0 */
+
+
 /* @brief Find the unknown function u at final time t = 1 in the evolution problem
  * using Galerkin discretization and convolution quadrature (BDF-2)
- * \param phi Template function for the right-hand side
+ * \param g Template function for the right-hand side
  * \param M Number of discretization intervals in time
  * \param N Number of discretization intervals in space
  * \param p Order of quadrature rule
  * \\return Values of u at final time t = 1
  */
-/* SAM_LISTING_BEGIN_0 */
+/* SAM_LISTING_BEGIN_1 */
 template<typename FUNC>
-VectorXd solveABC(const FUNC& phi, size_t M, size_t N, int p)
+VectorXd solveABC(const FUNC& g, size_t M, size_t N, int p)
 {
     // TODO: Find the unknown function u at final time t = 1 in the evolution problem
 }
-/* SAM_LISTING_END_0 */
+/* SAM_LISTING_END_1 */
 
 
 int main() {
-    /* SAM_LISTING_BEGIN_1 */
+    /* SAM_LISTING_BEGIN_2 */
     // TODO: Tabulate the H1-error of the Galerkin discretization + convolution quadrature
-    /* SAM_LISTING_END_1 */
+    /* SAM_LISTING_END_2 */
 }
