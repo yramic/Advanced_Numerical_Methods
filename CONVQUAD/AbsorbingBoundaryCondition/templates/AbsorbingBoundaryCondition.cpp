a86077b39e73287162b74830ddea96038f620fe7
Pratyuksh Bansal <pratyuksh.bansal@gmail.com>
2018-11-30 12:10:03 +0100

    Fix bugs, update problem 3, chapter 3

--- CONVQUAD/AbsorbingBoundaryCondition/templates/AbsorbingBoundaryCondition.cpp
+++ CONVQUAD/AbsorbingBoundaryCondition/templates/AbsorbingBoundaryCondition.cpp
@@ -9,12 +9,13 @@ using namespace Eigen;
 using namespace std;
 
 
-/* @brief Compute the convolution weights for Laplace transform F
+
+/* @brief Compute the BDF-2 convolution weights for Laplace transform F
  * \param F Template function for the Laplace transform
- * \param n Number of convolution weights, minus 1
+ * \param n Number of convolution weights, plus 1
  * \param p Order of quadrature rule
  * \param r Radius of circumference of integration (default = 1.0E-7)
- * \\return Values of u at final time t = 1
+ * \\return BDF-2 convolution weights
  */
 /* SAM_LISTING_BEGIN_0 */
 template<typename FFUNC>
@@ -25,6 +26,9 @@ VectorXd conv_wght_bdf2(const FFUNC& F, size_t n, int p, double r=1.0E-7)
 /* SAM_LISTING_END_0 */
 
 
+
+
+
 /* @brief Find the unknown function u at final time t = 1 in the evolution problem
  * using Galerkin discretization and convolution quadrature (BDF-2)
  * \param g Template function for the right-hand side
@@ -33,17 +37,17 @@ VectorXd conv_wght_bdf2(const FFUNC& F, size_t n, int p, double r=1.0E-7)
  * \param p Order of quadrature rule
  * \\return Values of u at final time t = 1
  */
-/* SAM_LISTING_BEGIN_1 */
+/* SAM_LISTING_BEGIN_2 */
 template<typename FUNC>
-VectorXd solveABC(const FUNC& g, size_t M, size_t N, int p)
+VectorXd solve_IBVP(const FUNC& g, size_t M, size_t N, int p)
 {
     // TODO: Find the unknown function u at final time t = 1 in the evolution problem
 }
-/* SAM_LISTING_END_1 */
+/* SAM_LISTING_END_2 */
 
 
 int main() {
-    /* SAM_LISTING_BEGIN_2 */
+    /* SAM_LISTING_BEGIN_3 */
     // TODO: Tabulate the H1-error of the Galerkin discretization + convolution quadrature
-    /* SAM_LISTING_END_2 */
+    /* SAM_LISTING_END_3 */
 }
