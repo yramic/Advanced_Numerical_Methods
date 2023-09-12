f5dcd49e42d0d249bace0b6885791f507374e25f
Bob Schreiner <bobschreiner@student-net-vpn-0471.intern.ethz.ch>
2023-09-12 17:26:37 +0200

    deployed

---
+++ HMAT/GravitationalForces/mastersolution/test/gravitationalforces_test.cpp
@@ -0,0 +1,15 @@
+/**
+* @file gravitationalforces_test.cpp
+* @brief NPDE homework GravitationalForces test code
+* @author
+* @date
+* @copyright Developed at SAM, ETH Zurich
+*/
+
+
+#include <gtest/gtest.h>
+#include "../gravitationalforces.h"
+
+namespace GravitationalForces{
+
+} // namespace GravitationalForces
