cbd525f34f25154a847576b22689237791ae4be9
ralfh <ralfh@ethz.ch>
2023-09-24 11:01:17 +0200

    Aded files for EvalTrigPoly

---
+++ HMAT/EvalTrigPoly/mastersolution/dependencies.cmake
@@ -0,0 +1,10 @@
+set(SOURCES
+        ${DIR}/evaltrigpoly.h
+        ${DIR}/evaltrigpoly.cpp
+        ${DIR}/evaltrigpoly_main.cpp
+        )
+
+set(LIBRARIES
+        Eigen3::Eigen
+        Clustering
+        )
