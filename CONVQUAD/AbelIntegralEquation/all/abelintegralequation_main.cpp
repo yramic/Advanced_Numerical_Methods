99a57676bde67fc84a5ff1118d1624171c0bdcd1
Jonas Bachmann <jbachmann@ethz.ch>
2023-09-29 11:56:38 +0200

    polished poly_spec_abel and added std:: and Eigen::

--- CONVQUAD/AbelIntegralEquation/all/abelintegralequation_main.cpp
+++ CONVQUAD/AbelIntegralEquation/all/abelintegralequation_main.cpp
@@ -7,29 +7,29 @@ int main() {
     auto y = [](double t) { return t; };
 
     double tau = 0.01;
-    size_t N = round(1. / tau);
-    VectorXd grid = VectorXd::LinSpaced(N + 1, 0., 1.);
-    VectorXd u_ex(N + 1);
+    const std::size_t N = round(1. / tau);
+    const Eigen::VectorXd grid = Eigen::VectorXd::LinSpaced(N + 1, 0., 1.);
+    Eigen::VectorXd u_ex(N + 1);
     for (int i = 0; i < N + 1; ++i) {
       u_ex(i) = u(grid(i));
     }
 
 #if SOLUTION
-    cout << "\nSpectral Galerkin\n" << endl;
+    std::cout << "\nSpectral Galerkin\n" << std::endl;
     double err_max, err_max_alt;
     for (int p = 2; p <= 10; ++p) {
-      VectorXd u_app = AbelIntegralEquation::poly_spec_abel(y, p, tau);
-      VectorXd diff = u_ex - u_app;
+      Eigen::VectorXd u_app = AbelIntegralEquation::poly_spec_abel(y, p, tau);
+      Eigen::VectorXd diff = u_ex - u_app;
       err_max = diff.cwiseAbs().maxCoeff();
       double dp = p;
       if (p == 2) {
-        cout << "p = " << p << setw(15) << "Max = " << scientific
-             << setprecision(3) << err_max << endl;
+        std::cout << "p = " << p << std::setw(15) << "Max = " << std::scientific
+             << std::setprecision(3) << err_max << std::endl;
       } else {
-        cout << "p = " << p << setw(15) << "Max = " << scientific
-             << setprecision(3) << err_max << setw(15) << " EOC = "
+        std::cout << "p = " << p << std::setw(15) << "Max = " << std::scientific
+             << std::setprecision(3) << err_max << std::setw(15) << " EOC = "
              << std::log2(err_max_alt / err_max) / std::log2((dp + 1) / dp)
-             << endl;
+             << std::endl;
       }
       err_max_alt = err_max;
     }
@@ -51,45 +51,45 @@ int main() {
     double err_max, err_max_alt;
     cout << "\n\nConvolution Quadrature, Implicit Euler\n" << endl;
     for (int N = 16; N <= 2048; N *= 2) {
-      VectorXd grid = VectorXd::LinSpaced(N + 1, 0., 1.);
-      VectorXd u_ex(N + 1);
+      Eigen::VectorXd grid = Eigen::VectorXd::LinSpaced(N + 1, 0., 1.);
+      Eigen::VectorXd u_ex(N + 1);
       for (int i = 0; i < N + 1; ++i) {
         u_ex(i) = u(grid(i));
       }
 
-      VectorXd u_app = AbelIntegralEquation::cq_ieul_abel(y, N);
-      VectorXd diff = u_ex - u_app;
+      Eigen::VectorXd u_app = AbelIntegralEquation::cq_ieul_abel(y, N);
+      Eigen::VectorXd diff = u_ex - u_app;
       err_max = diff.cwiseAbs().maxCoeff();
       if (N == 16) {
-        cout << "N = " << N << setw(15) << "Max = " << scientific
-             << setprecision(3) << err_max << endl;
+        std::cout << "N = " << N << std::setw(15) << "Max = " << std::scientific
+             << std::setprecision(3) << err_max << std::endl;
       } else {
-        cout << "N = " << N << setw(15) << "Max = " << scientific
-             << setprecision(3) << err_max << setw(15)
-             << " EOC = " << std::log2(err_max_alt / err_max) << endl;
+        std::cout << "N = " << N << std::setw(15) << "Max = " << std::scientific
+             << std::setprecision(3) << err_max << std::setw(15)
+             << " EOC = " << std::log2(err_max_alt / err_max) << std::endl;
       }
 
       err_max_alt = err_max;
     }
 
-    cout << "\n\nConvolution Quadrature, BDF-2\n" << endl;
+    std::cout << "\n\nConvolution Quadrature, BDF-2\n" << std::endl;
     for (int N = 16; N <= 2048; N *= 2) {
-      VectorXd grid = VectorXd::LinSpaced(N + 1, 0., 1.);
-      VectorXd u_ex(N + 1);
+      Eigen::VectorXd grid = Eigen::VectorXd::LinSpaced(N + 1, 0., 1.);
+      Eigen::VectorXd u_ex(N + 1);
       for (int i = 0; i < N + 1; ++i) {
         u_ex(i) = 2. / M_PI * sqrt(grid(i));
       }
 
-      VectorXd u_app = AbelIntegralEquation::cq_bdf2_abel(y, N);
-      VectorXd diff = u_ex - u_app;
+      Eigen::VectorXd u_app = AbelIntegralEquation::cq_bdf2_abel(y, N);
+      Eigen::VectorXd diff = u_ex - u_app;
       err_max = diff.cwiseAbs().maxCoeff();
       if (N == 16) {
-        cout << "N = " << N << setw(15) << "Max = " << scientific
-             << setprecision(3) << err_max << endl;
+        std::cout << "N = " << N << std::setw(15) << "Max = " << std::scientific
+             << std::setprecision(3) << err_max << std::endl;
       } else {
-        cout << "N = " << N << setw(15) << "Max = " << scientific
-             << setprecision(3) << err_max << setw(15)
-             << " EOC = " << std::log2(err_max_alt / err_max) << endl;
+        cout << "N = " << N << std::setw(15) << "Max = " << std::scientific
+             << std::setprecision(3) << err_max << std::setw(15)
+             << " EOC = " << std::log2(err_max_alt / err_max) << std::endl;
       }
 
       err_max_alt = err_max;
