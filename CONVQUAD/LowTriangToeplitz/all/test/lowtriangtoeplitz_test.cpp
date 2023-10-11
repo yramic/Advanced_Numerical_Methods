e95b2be8e26f26ca0fa223697e1d90875b4946d9
Bob Schreiner <bobschreiner@student-net-vpn-2690.intern.ethz.ch>
2023-10-11 17:59:39 +0200

    Minor cleanups in output, added one test that is failing check for correctness

--- CONVQUAD/LowTriangToeplitz/all/test/lowtriangtoeplitz_test.cpp
+++ CONVQUAD/LowTriangToeplitz/all/test/lowtriangtoeplitz_test.cpp
@@ -12,11 +12,32 @@
 
 namespace LowTriangToeplitz::test {
 
-TEST(LowTriangToeplitz, toeplitz) {
-  Eigen::VectorXcd u(3);
-  u << 1, 2, 3;
+Eigen::VectorXcd ltpMultold(const Eigen::VectorXcd& f,
+                            const Eigen::VectorXcd& g) {
+  assert(f.size() == g.size() && "f and g vectors must have the same length!");
 
-  ASSERT_NEAR((ltpMultold(u, u) - ltpMult(u, u)).real().norm(), 0.0, 1e-8);
+  const std::size_t n = f.size();
+  return toepMatVecMult(f, Eigen::VectorXcd::Zero(n), g);
+}
+
+TEST(LowTriangToeplitz, ltpMult) {
+  const Eigen::VectorXcd u = Eigen::VectorXcd::Random(10);
+  const Eigen::VectorXcd v = Eigen::VectorXcd::Random(10);
+  ASSERT_NEAR((ltpMultold(u, v) - ltpMult(u, v)).real().norm(), 0.0, 1e-8);
+}
+
+TEST(LowTriangToeplitz, ltpSolve) {
+  const Eigen::VectorXcd u = Eigen::VectorXcd::Random(16);
+  const Eigen::VectorXcd v = Eigen::VectorXcd::Random(16);
+  Eigen::VectorXcd zero(16);
+  zero[0] = u[0];
+  zero.tail(15) = Eigen::VectorXcd::Zero(15);
+  const Eigen::MatrixXcd K = toeplitz(u, zero);
+  const Eigen::VectorXcd res = K.llt().solve(v);
+  const Eigen::VectorXcd stud_res = ltpSolve(u, v);
+  std::cout << res - stud_res << std::endl;
+  //std::cout << stud_res << std::endl;
+  ASSERT_NEAR((res - stud_res).real().norm(), 0.0, 1e-8);
 }
 
 }  // namespace LowTriangToeplitz::test
